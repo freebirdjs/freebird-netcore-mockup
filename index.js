e22a4a20c46f95e4944d4f8b53b182c31f85a802
Simen Li <simenkid@gmail.com>
2017-01-11 17:12:47 +0800

    update to lastest libs - should write test after FB classes are all done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var lwm2mId = require('lwm2m-id'),
     FreebirdBase = require('freebird-base'),
-    Netcore = FreebirdBase.Netcore,
     controller = require('./lib/controller.js');
 
 var EventEmitter = require('events');
@@ -16,34 +15,34 @@ var validGads = [
 /*** Drivers                                                         ***/
 /***********************************************************************/
 var netDrivers = {
-    start: controller.start,            // function(callback) {}, callback(err)
-    stop: controller.stop,              // function(callback) {}, callback(err)
-    reset: controller.reset,            // function(mode, callback) {}, callback(err)
-    permitJoin: controller.permitJoin,  // function(duration, callback) {}, callback(err, timeLeft) (Number, ex: 180)
-    remove: controller.remove,          // function(permAddr, callback) {}, callback(err, permAddr) (String, ex: '0x12345678')
-    ban: controller.ban,                // function(permAddr, callback) {}, callback(err, permAddr) (String, ex: '0x12345678')
-    unban: controller.unban,            // function(permAddr, callback) {}, callback(err, permAddr) (String, ex: '0x12345678')
-    ping: controller.ping               // function(permAddr, callback) {}, callback(err, time)     (Number, ex: 16)
+    start: controller.start,            // function(done) {}, done(err)
+    stop: controller.stop,              // function(done) {}, done(err)
+    reset: controller.reset,            // function(mode, done) {}, done(err)
+    permitJoin: controller.permitJoin,  // function(duration, done) {}, done(err, timeLeft) (Number, ex: 180)
+    remove: controller.remove,          // function(permAddr, done) {}, done(err, permAddr) (String, ex: '0x12345678')
+    ban: controller.ban,                // function(permAddr, done) {}, done(err, permAddr) (String, ex: '0x12345678')
+    unban: controller.unban,            // function(permAddr, done) {}, done(err, permAddr) (String, ex: '0x12345678')
+    ping: controller.ping               // function(permAddr, done) {}, done(err, time)     (Number, ex: 16)
 };
 
 var devDrivers = {
-    read: controller.devRead,           // function(permAddr, attr, callback) {},       callback(err, val)  (Type denpends, ex: 'hello', 12, false)
-    write: controller.devWrite,         // function(permAddr, attr, val, callback) {},  callback(err, val)  (optional, Type denpends, ex: 'hello', 12, false)
-    identify: controller.devIdentify,   // function(permAddr, callback) {},             callback(err)
+    read: controller.devRead,           // function(permAddr, attr, done) {},       done(err, val)  (Type denpends, ex: 'hello', 12, false)
+    write: controller.devWrite,         // function(permAddr, attr, val, done) {},  done(err, val)  (optional, Type denpends, ex: 'hello', 12, false)
+    identify: controller.devIdentify,   // function(permAddr, done) {},             done(err)
 };
 
 var gadDrivers = {
-    read: controller.gadRead,               // function(permAddr, auxId, attr, callback) {},          callback(err, val)    (Type denpends, ex: 'hello', 12, false)
-    write: controller.gadWrite,             // function(permAddr, auxId, attr, val, callback) {},     callback(err, val)    (optional, Type denpends, ex: 'hello', 12, false)
-    exec: controller.gadExec,               // function(permAddr, auxId, attr, args, callback) {},    callback(err, result) (can be anything, depends on firmware implementation)
-    setReportCfg: controller.setReportCfg,  // function(permAddr, auxId, attrName, cfg, callback) {}, callback(err, result) (set succeeds? (Boolean, true or false))
-    getReportCfg: controller.getReportCfg,  // function(permAddr, auxId, attrName, callback) {},      callback(err, cfg)    (Object, ex: { pmin: 10, pmax: 60, gt: 200 })
+    read: controller.gadRead,                   // function(permAddr, auxId, attr, done) {},          done(err, val)    (Type denpends, ex: 'hello', 12, false)
+    write: controller.gadWrite,                 // function(permAddr, auxId, attr, val, done) {},     done(err, val)    (optional, Type denpends, ex: 'hello', 12, false)
+    exec: controller.gadExec,                   // function(permAddr, auxId, attr, args, done) {},    done(err, result) (can be anything, depends on firmware implementation)
+    writeReportCfg: controller.writeReportCfg,  // function(permAddr, auxId, attrName, cfg, done) {}, done(err, result) (set succeeds? (Boolean, true or false))
+    readReportCfg: controller.readReportCfg,    // function(permAddr, auxId, attrName, done) {},      done(err, cfg)    (Object, ex: { pmin: 10, pmax: 60, gt: 200 })
 };
 
 /***********************************************************************/
 /*** Start Building Netcore                                          ***/
 /***********************************************************************/
-var nc = new Netcore('mock', controller, {
+var nc = FreebirdBase.createNetcore('mock', controller, {
     phy: 'test_phy',
     nwk: 'test_nwk'
 });
@@ -52,9 +51,9 @@ nc.registerNetDrivers(netDrivers);
 nc.registerDevDrivers(devDrivers);
 nc.registerGadDrivers(gadDrivers);
 
-nc.cookRawDev = function (dev, rawDev, cb) {
+nc._cookRawDev = function (dev, rawDev, done) {
 
-    dev.setNetInfo({
+    dev.set('net', {
         role: 'end-device',
         parent: '0',
         maySleep: true,
@@ -65,7 +64,7 @@ nc.cookRawDev = function (dev, rawDev, cb) {
         }
     });
 
-    dev.setAttrs({
+    dev.set('attrs', {
         manufacturer: rawDev.so.device[0].manuf,
         model: rawDev.so.device[0].model,
         serial: '',
@@ -82,28 +81,28 @@ nc.cookRawDev = function (dev, rawDev, cb) {
 
     dev.extra = rawDev;
 
-    process.nextTick(function () {
-        cb(null, dev);
+    setImmediate(function () {
+        done(null, dev);
     });
 };
 
-nc.cookRawGad = function (gad, rawGad, cb) {
+nc._cookRawGad = function (gad, rawGad, done) {
     var gadId = Object.keys(rawGad)[0],
         gadClass = getGadClass(gadId);
 
     if (!gadClass) {
         process.nextTick(function () {
-            cb(new Error('No matched gadget'));
+            done(new Error('No matched gadget'));
         });
     } else {
-        gad.setPanelInfo({
+        gad.set('panel', {
             profile: 'none',
             classId: gadClass
         });
 
-        gad.setAttrs(rawGad[gadId]);
-        process.nextTick(function () {
-            cb(null, gad);
+        gad.set('attrs', rawGad[gadId]);
+        setImmediate(function () {
+            done(null, gad);
         });
     }
 };
@@ -115,7 +114,7 @@ controller.on('SYS_READY', function () {
 
 controller.on('DEV_INCOMING', function (dev) {
     console.log('** DEV INCOMING **');
-    nc.commitDevIncoming(dev.mac, dev);
+    nc.commitDevIncoming(dev.mac, dev); // function (permAddr, rawDev)
 
     for (var k in dev.so) {
         var rawGad = {};
@@ -125,7 +124,7 @@ controller.on('DEV_INCOMING', function (dev) {
             for (var iid in dev.so[k]) {
                 var auxId = oidStr + '/' + iid;
                 rawGad[k] = dev.so[k][iid];
-                nc.commitGadIncoming(dev.mac, auxId, rawGad);
+                nc.commitGadIncoming(dev.mac, auxId, rawGad);   // function (permAddr, auxId, rawGad)
             }
         }
     }
@@ -152,16 +151,23 @@ controller.on('GAD_REPORTING', function (gadChanges) {
     // gadChanges = { dev, oid, iid, rid, data }
     var dev = gadChanges.dev,
         attrData = gadChanges.data;
-    var oidStr = lwm2mId.getOid(gadChanges.oid).key,
-        ridStr = lwm2mId.getRid(gadChanges.oid, gadChanges.rid).key,
-        auxId = oidStr + '/' + gadChanges.iid;
 
-    // var ridsToGet = [ 'manuf', 'model', 'hwVer', 'swVer', 'availPwrSrc', 'pwrSrcVoltage' ];
+    var oidStr = lwm2mId.getOid(gadChanges.oid),
+        ridStr = lwm2mId.getRid(gadChanges.oid, gadChanges.rid),
+        auxId;
 
     var devAttrs = {},
         gadAttrs = {};
 
-    if (oidStr === 'device' && gadChanges.iid === 0) {
+    if (!oidStr || !ridStr)
+        return; // ignore
+
+    oidStr = oidStr.key;
+    ridStr = ridStr.key;
+    auxId = oidStr + '/' + gadChanges.iid;
+
+    // ridsToGet = [ 'manuf', 'model', 'hwVer', 'swVer', 'availPwrSrc', 'pwrSrcVoltage' ]
+    if (oidStr === 'device' &&  parseInt(gadChanges.iid) === 0) {
         switch (ridStr) {
             case 'manuf':
                 devAttrs.manufacturer = attrData;
@@ -189,6 +195,7 @@ controller.on('GAD_REPORTING', function (gadChanges) {
 
         if (Object.keys(devAttrs).length)
             nc.commitDevReporting(dev.mac, devAttrs);
+
     } else if (getGadClass(oidStr)) {
         gadAttrs[ridStr] = attrData;
         nc.commitGadReporting(dev.mac, auxId, gadAttrs);
